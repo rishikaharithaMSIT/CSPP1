2b80bf103ad4a4cdc19c8609ee2e7d2336e9e4c7
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-01 18:52:54 +0530

    added sum prog

---
+++ cspp1-practice/m3/sum_1_end.py
@@ -0,0 +1,17 @@
+'''
+Author: Rishika Haritha - 20186041
+Date: 1-August-2018
+Encoding: Utf-8
+'''
+
+#sum of integers from 1 to end
+END = int(input())
+
+I = 1
+SUM = 0
+
+while I <= END:
+    SUM = SUM + I
+    I += 1
+
+print(SUM)
