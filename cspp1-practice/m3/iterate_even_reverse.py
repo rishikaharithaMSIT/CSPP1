3d6e05d8e0113344702ff4c0592675ef22db5a01
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-01 18:40:22 +0530

    added iterate_even_reverse.py

---
+++ cspp1-practice/m3/iterate_even_reverse.py
@@ -0,0 +1,13 @@
+'''
+Author: Rishika Haritha - 20186041
+Date: 1-August-2018
+Encoding: Utf-8
+'''
+
+#program to print even numbers in reverse order
+
+print("Hello!")
+I = 10
+while I >= 2:
+    print(I)
+    I -= 2
