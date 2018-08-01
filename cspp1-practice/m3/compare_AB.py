968a0cd4b6844a77d3bd889bf6097625b65f7147
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-01 17:40:25 +0530

    added iterate_even.py

---
+++ cspp1-practice/m3/compare_AB.py
@@ -0,0 +1,11 @@
+'''
+Author: Rishika Haritha - 20186041
+Date: 1-August-2018
+Encoding: Utf-8
+'''
+
+INPUT1 = input()
+INPUT2 = input()
+
+for i in INPUT:
+    
---
+++ cspp1-practice/m3/iterate_even.py
@@ -0,0 +1,9 @@
+'''
+Author: Rishika Haritha - 20186041
+Date: 1-August-2018
+Encoding: Utf-8
+'''
+
+for i in range(2, 11, 2):
+    print(i)
+print("Goodbye!")
