31cd0fa1f2718f88037a284ef2e17f8b0fa614a2
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-02 17:11:56 +0530

    " -> 5 of 5 passed. pylint: 8.00/10 "

--- cspp1-assignments/m4/p2/bob_counter.py
+++ cspp1-assignments/m4/p2/bob_counter.py
@@ -10,15 +10,15 @@ For example, if s = 'azcbobobegghakl', then your program should print
 Number of times bob occurs is: 2'''
 
 def main():
-	'''This is main function'''
-	input_string = input()
-	# the input string is in s
-	# remove pass and start your code here
-	count_bob = 0 
-	for i in range(len(input_string)-2):
-		if input_string[i:i+3] == 'bob':
-			count_bob += 1
-	print(count_bob)
+    '''This is main function'''
+    input_string = input()
+    # the input string is in s
+    # remove pass and start your code here
+    count_bob = 0 
+    for i in range(len(input_string)-2):
+        if input_string[i:i+3] == 'bob':
+            count_bob += 1
+    print(count_bob)
 
 if __name__ == "__main__":
-	main()
+    main()
