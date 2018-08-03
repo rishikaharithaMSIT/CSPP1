baf97dafa7fde9df30aa57807b09d99fec86dc72
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-03 18:15:42 +0530

    " -> 3 of 3 passed. pylint: 7.14/10 "

--- cspp1-assignments/m5/p3/square_root_bisection.py
+++ cspp1-assignments/m5/p3/square_root_bisection.py
@@ -1,3 +1,8 @@
+'''
+Author: Rishika Haritha - 20186041
+Encoding: Utf-8
+'''
+
 # Write a python program to find the square root of the given number
 # using approximation method
 
@@ -10,23 +15,23 @@
 # output: 6.999999999999991
 
 def main():
-	square_num = int(input())
-	# epsilon and step are initialized
-	# don't change these values
-	approximation_val = 0.01
-	# your code starts here
-	high_val = square_num
-	low_val = 0
-	middle_val = (low_val + high_val) / 2
+    square_num = int(input())
+    # epsilon and step are initialized
+    # don't change these values
+    approximation_val = 0.01
+    # your code starts here
+    high_val = square_num
+    low_val = 0
+    middle_val = (low_val + high_val) / 2
 
-	while abs((middle_val ** 2) - square_num) >= approximation_val:
-		if square_num > middle_val ** 2:
-			low_val = middle_val			
-		else:
-			high_val = middle_val
-		middle_val = (low_val + high_val) / 2	
-	print(middle_val) 
+    while abs((middle_val ** 2) - square_num) >= approximation_val:
+        if square_num > middle_val ** 2:
+            low_val = middle_val            
+        else:
+            high_val = middle_val
+        middle_val = (low_val + high_val) / 2   
+    print(middle_val) 
 
 
 if __name__ == "__main__":
-	main()
+    main()
