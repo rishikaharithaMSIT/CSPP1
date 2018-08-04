308669fe97c45015a219203218cd5349343ff1ed
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-04 10:31:51 +0530

    " -> 7 of 7 passed. pylint: 5.00/10 "

---
+++ cspp1-assignments/m6/p2/special_char.py
@@ -0,0 +1,23 @@
+'''
+Author: Rishika Haritha - 20186041
+Encoding: Utf-8
+'''
+
+#Replace all the special characters(!, @, #, $, %, ^, &, *) in a given string with a space.
+
+def main():
+    '''
+    Read string from the input, store it in variable str_input.
+    '''
+    str_input = input()
+    output_str = ""
+    for str_iterator in range(len(str_input)):
+    	if str_input[str_iterator] in "!@#$%^&*":
+    		output_str += " "
+    	else:
+    		output_str += str_input[str_iterator]
+
+    print(output_str)
+
+if __name__ == "__main__":
+    main()
