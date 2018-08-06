60024d4ffe9dffd9b877dd148c6115923ec74e43
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-06 18:12:19 +0530

    " -> 4 of 10 passed. pylint: -0.40/10 "

--- cspp1-assignments/Functions - Assignment-2/assignment2.py
+++ cspp1-assignments/Functions - Assignment-2/assignment2.py
@@ -42,7 +42,7 @@ def payingDebtOffInAYear(balance, annualInterestRate):
         guess_num += step_val
          
     
-    m = guess_num
+    m = round(guess_num)
     return "Lowest Payment: "+str(m*10)
     
     
