105ce4e486e56a450688ed8deb3bce13e58c4287
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-07 19:23:13 +0530

    " -> 10 of 10 passed. pylint: 8.89/10 "

---
+++ cspp1-assignments/m8/p2/assignment2.py
@@ -0,0 +1,24 @@
+'''
+Author: Rishika Haritha - 20186041
+Encoding: Utf-8
+'''
+
+def sum_of_digits(n):
+    '''
+    n is positive Integer
+
+    returns: a positive integer, the sum of digits of n.
+    '''
+    # Your code here
+    if n == 0:
+    	return 0
+    return n % 10 + sumofdigits(n // 10)
+
+
+def main():
+    '''main function'''
+    a = input()
+    print(sum_of_digits(int(a)))
+
+if __name__ == "__main__":
+    main()
