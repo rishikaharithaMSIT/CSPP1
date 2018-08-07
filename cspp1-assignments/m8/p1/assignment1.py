6ae61bd062ee8d099181f2b4ea5b57707ba0a4ae
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-07 20:51:39 +0530

    " -> 10 of 10 passed. pylint: 10.00/10 "

--- cspp1-assignments/m8/p1/assignment1.py
+++ cspp1-assignments/m8/p1/assignment1.py
@@ -9,7 +9,7 @@ def factorial(input_num):
     returns: a positive integer, the factorial of n.
     '''
     # Your code here
-    if input_num in (0,1):
+    if input_num in (0, 1):
         return 1
     return input_num * factorial(input_num - 1)
 def main():
