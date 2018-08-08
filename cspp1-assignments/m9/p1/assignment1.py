65c13642f566cfa7d98095e77d26b6e95fe43657
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-08 20:11:30 +0530

    " -> 9 of 9 passed. pylint: 9.17/10 "

--- cspp1-assignments/m9/p1/assignment1.py
+++ cspp1-assignments/m9/p1/assignment1.py
@@ -11,14 +11,14 @@ def is_word_guessed(secret_word, letters_guessed):
     secret_word = list(secret_word)
     for secret_letter in letters_guessed:
         if secret_letter in secret_word:
-            lambda_solution = filter(lambda lambda_params: 
+            lambda_solution = filter(lambda lambda_params:
                                      lambda_params != secret_letter, secret_word)
             secret_word = list(lambda_solution)
         if len(secret_word) == 0:
             return "True"
     if len(secret_word) is 0:
         return "True"
-    return "False"   
+    return "False"
 def main():
     '''
     Main function for the program
