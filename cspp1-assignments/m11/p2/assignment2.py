4f6fdedf41cc1c2c0db48c43fe0ea295b7ed9b28
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-10 18:16:54 +0530

    " -> 6 of 6 passed. pylint: 10.00/10 "

--- cspp1-assignments/m11/p2/assignment2.py
+++ cspp1-assignments/m11/p2/assignment2.py
@@ -8,7 +8,7 @@ def update_hand(player_hand, input_word):
     Assumes that 'hand' has all the letters in word.
     In other words, this assumes that however many times
     a letter appears in 'word', 'hand' has at least as
-    many of that letter in it. 
+    many of that letter in it.
 
     Updates the hand: uses up the letters in the given word
     and returns the new hand, without those letters in it.
@@ -16,7 +16,7 @@ def update_hand(player_hand, input_word):
     Has no side effects: does not modify hand.
 
     word: string
-    hand: dictionary (string -> int)    
+    hand: dictionary (string -> int)
     returns: dictionary (string -> int)
     """
     # TO DO ... <-- Remove this comment when you code this function
