ef981db2adf241f4486cb78213e0f35faa5edefd
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-10 18:22:17 +0530

    " -> 3 of 3 passed. pylint: 7.31/10 "

--- cspp1-assignments/m11/p3/assignment3.py
+++ cspp1-assignments/m11/p3/assignment3.py
@@ -19,15 +19,15 @@ def is_validword(word, hand, wordList):
     is_in_wordlist = False
     flag = 0
     for w in word:
-    	if w not in hand:
-    		flag = 1
+        if w not in hand:
+            flag = 1
     if not flag:
-    	is_in_hand = True
+        is_in_hand = True
     if word in wordList:
-    	is_in_wordlist = True
+        is_in_wordlist = True
 
     if is_in_hand and is_in_wordlist:
-    	return True
+        return True
     return False
 
 
@@ -36,17 +36,17 @@ def is_validword(word, hand, wordList):
 
 def main():
     '''in main function'''
-	input_word = input()
-	test_cases = int(input())
-	a_dict={}
-	for i in range(test_cases):
-		da_ta = input()
-		l_1 = da_ta.split()
-		a_dict[l_1[0]] = int(l_1[1])
-	l_2 = input().split()
-	print(is_validword(input_word, a_dict, l_2))
-		
+    input_word = input()
+    test_cases = int(input())
+    a_dict={}
+    for i in range(test_cases):
+        da_ta = input()
+        l_1 = da_ta.split()
+        a_dict[l_1[0]] = int(l_1[1])
+    l_2 = input().split()
+    print(is_validword(input_word, a_dict, l_2))
+        
 
 
 if __name__ == "__main__":
-	main()
+    main()
