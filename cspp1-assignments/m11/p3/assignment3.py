79d7f5d7b952198f960cef0d86885d4979fe3ed0
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-10 18:20:43 +0530

    " -> 6 of 6 passed. pylint: 10.00/10 "

--- cspp1-assignments/m11/p3/assignment3.py
+++ cspp1-assignments/m11/p3/assignment3.py
@@ -1,13 +1,9 @@
-# Assignment-3
 '''
-At this point, we have written code to generate a random hand and display that hand to the user. We can also ask the user for a word (Python's input) and score the word (using your getWordScore). However, at this point we have not written any code to verify that a word given by a player obeys the rules of the game. A valid word is in the word list; and it is composed entirely of letters from the current hand. Implement the isValidWord function.
-
-Testing: Make sure the test_isValidWord tests pass. In addition, you will want to test your implementation by calling it multiple times on the same hand - what should the correct behavior be? Additionally, the empty string ('') is not a valid word - if you code this function correctly, you shouldn't need an additional check for this condition.
-
-Fill in the code for isValidWord in ps4a.py and be sure you've passed the appropriate tests in test_ps4a.py before pasting your function definition here.
+Author: Rishika Haritha - 20186041
+Encoding: Utf-8
 '''
 
-def isValidWord(word, hand, wordList):
+def is_validword(word, hand, wordList):
     """
     Returns True if word is in the wordList and is entirely
     composed of letters in the hand. Otherwise, returns False.
@@ -39,17 +35,18 @@ def isValidWord(word, hand, wordList):
 
 
 def main():
-	word=input()
-	n=int(input())
-	adict={}
-	for i in range(n):
-		data=input()
-		l=data.split()
-		adict[l[0]]=int(l[1])
-	l2=input().split()
-	print(isValidWord(word,adict,l2))
+    '''in main function'''
+	input_word = input()
+	test_cases = int(input())
+	a_dict={}
+	for i in range(test_cases):
+		da_ta = input()
+		l_1 = da_ta.split()
+		a_dict[l_1[0]] = int(l_1[1])
+	l_2 = input().split()
+	print(is_validword(input_word, a_dict, l_2))
 		
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
 	main()
