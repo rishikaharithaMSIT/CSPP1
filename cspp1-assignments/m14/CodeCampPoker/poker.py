b15ef0c7c5c8a4f43741eeeb181564344507e17f
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-14 12:19:25 +0530

    " -> 3 of 3 passed. pylint: 9.68/10 "

--- cspp1-assignments/m14/CodeCampPoker/poker.py
+++ cspp1-assignments/m14/CodeCampPoker/poker.py
@@ -104,12 +104,11 @@ def hand_rank(hand):
 
     if is_straight(hand_val) and is_flush(hand_suit):
         return 3
-    elif is_straight(hand_val):
+    if is_straight(hand_val):
         return 1
-    elif is_flush(hand_suit):
+    if is_flush(hand_suit):
         return 2
-    else:
-        return 0
+    return 0
     
 
 def poker(hands):
