cbfd5acc656a60ee5e5c658509464ce39a30167b
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-16 11:15:59 +0530

    " -> 0 of 19 passed. pylint: 9.01/10 "

--- cspp1-assignments/m14/CodeCampPoker/poker.py
+++ cspp1-assignments/m14/CodeCampPoker/poker.py
@@ -143,7 +143,7 @@ def is_same(com_list):
     s_rank = []
     for i in com_list:
         if max_r == com_list[i]:
-            l = i.strip("[]")
+            l = i.strip("['']")
             l = l.split(",")
             for i in l:
                 i = i.strip(" ''")
