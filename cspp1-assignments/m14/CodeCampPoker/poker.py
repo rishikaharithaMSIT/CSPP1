c565927ec6642f6dfdd1c4801d96b367c5c4ace7
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-16 11:06:49 +0530

    " -> 0 of 19 passed. pylint: 8.83/10 "

--- cspp1-assignments/m14/CodeCampPoker/poker.py
+++ cspp1-assignments/m14/CodeCampPoker/poker.py
@@ -178,7 +178,7 @@ def hand_rank(hand):
     # third would be a straight with the return value 1
     # any other hand would be the fourth best with the return value 0
     # max in poker function uses these return values to select the best hand
-    count = 0
+    global count 
     hand_val = []
     len_hand = len(hand)
     for i in range(len_hand):
