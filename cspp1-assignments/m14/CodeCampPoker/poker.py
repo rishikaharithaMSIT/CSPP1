841ab99190b7eb75ae8bd43614938027694313e6
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-16 14:32:26 +0530

    " -> 14 of 19 passed. pylint: 7.77/10 "

--- cspp1-assignments/m14/CodeCampPoker/poker.py
+++ cspp1-assignments/m14/CodeCampPoker/poker.py
@@ -129,17 +129,17 @@ def is_no_same(hands):
         for k in range(len(hands[j])):
             hand_val.append(hands[j][k])
         for i in range(len_hand):
-            if hand[i] == 'A':
-                hand[i] = 14
-            if hand[i] == 'J':
-                hand[i] = 11
-            if hand[i] == 'Q':
-                hand[i] = 12
-            if hand[i] == 'K':
-                hand[i] = 13
-            if hand[i] == 'T':
-                hand[i] = 10
-            hand[i] = int(hand[i])
+            if hands[j][k] == 'A':
+                hands[j][k] = 14
+            if hands[j][k] == 'J':
+                hands[j][k] = 11
+            if hands[j][k] == 'Q':
+                hands[j][k] = 12
+            if hands[j][k] == 'K':
+                hands[j][k] = 13
+            if hands[j][k] == 'T':
+                hands[j][k] = 10
+            hands[j][k] = int(hands[j][k])
     #print(hand)
         ok.append(hand_val)
 
