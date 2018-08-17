f236c85ecded36a60fc92662711b3dfb7c47cb56
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-17 13:06:18 +0530

    " -> 3 of 3 passed. pylint: 9.14/10 "

--- cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
+++ cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
@@ -39,32 +39,32 @@ def remove_stop_words(in_freq):
         if each_key in stop_words:
             del in_freq_copy[each_key]
     return in_freq_copy
-
+def make_dicts_equal(freq_dict1,freq_dict2):
+    for each_key in freq_dict2:
+        if each_key not in freq_dict1:
+            freq_dict1[each_key] = 0
+    return freq_dict1
 def compute_similarity(freq_dict1, freq_dict2):
     '''compute similarity'''
     common_dict = {}
-    # for each_key in freq_dict1:
-    #     if each_key not in freq_dict2:
-    #         freq_dict2[each_key] = 0
-    # for each_key in freq_dict2:
-    #     if each_key not in freq_dict1:
-    #         freq_dict1[each_key] = 0
+    freq_dict1 = make_dicts_equal(freq_dict1,freq_dict2)
+    freq_dict2 = make_dicts_equal(freq_dict2,freq_dict1)
+       
 
     for each_word in freq_dict1:
         if each_word in common_dict:
-            common_dict[each_word][0] += 1
+            common_dict[each_word].append(freq_dict1[each_word])
         else:
-            common_dict[each_word] = [freq_dict1[each_word], 0]
+            common_dict[each_word] = [freq_dict1[each_word]]
     for each_word in freq_dict2:
         if each_word in common_dict:
-            common_dict[each_word][1] += 1
+            common_dict[each_word].append(freq_dict2[each_word])
         else:
-            common_dict[each_word] = [0,freq_dict2[each_word]]
+            common_dict[each_word] = [freq_dict2[each_word]]
     num_val = 0
     den_val = 1
     den_sum1 = 0
     den_sum2 = 0
-    
     #print(common_dict)
     #print(len(common_dict))
     common_dict_copy = copy.deepcopy(common_dict)
