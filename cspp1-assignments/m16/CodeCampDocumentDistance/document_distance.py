cef00e4ebdd3b3567a2b8ce8d0dd0e9781de44e4
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-17 10:55:34 +0530

    " -> 0 of 3 passed. pylint: 9.44/10 "

--- cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
+++ cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
@@ -66,7 +66,7 @@ def compute_similarity(freq_dict1, freq_dict2):
     print(common_dict)
     print(len(common_dict))
     for l in common_dict:
-        print(l)
+        print(len(l),"  ",l)
     for each_key in common_dict:
         #print(common_dict[each_key][0])
         num_val = num_val + (common_dict[each_key][0] * common_dict[each_key][1])
