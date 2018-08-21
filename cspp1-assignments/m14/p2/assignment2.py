42949b33d27f979c7fa7e2b01f7814a1f8a2e09c
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-21 23:43:30 +0530

    m14

--- cspp1-assignments/m14/p2/assignment2.py
+++ cspp1-assignments/m14/p2/assignment2.py
@@ -142,7 +142,7 @@ def main():
     print(shift)
     print(data.build_shift_dict(shift))
     print(data.apply_shift(shift))
-    print(data)
+    #print(data)
     shift = int(input())
     print(shift)
     print(data.build_shift_dict(shift))
