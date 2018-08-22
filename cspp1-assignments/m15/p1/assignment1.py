f8c970a2f1a34c52b49c25655bc996f04efc4d1d
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-22 20:26:55 +0530

    " -> 0 of 3 passed. pylint: 8.88/10 "

--- cspp1-assignments/m15/p1/assignment1.py
+++ cspp1-assignments/m15/p1/assignment1.py
@@ -253,7 +253,7 @@ class CiphertextMessage(Message):
         max_shift = 0
         for shift in range(27):
             cnt = 0
-            words = self.text.split()
+            words = self.message_text.split()
             for each_word in words:
                 message = Message(each_word)
                 self.encrypting_dict = message.build_shift_dict(shift)
