90dd145aeb6d073bb288ea711a3ab1693a234db9
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-23 09:31:41 +0530

    " -> 0 of 8 passed. pylint: 0.67/10 "

--- cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
+++ cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
@@ -36,7 +36,7 @@ def main():
     # add matrix 1 and mqtrix 2
 
     # multiply matrix 1 and matrix 2
-    matrix1_D = input().split(",")
+    matrix1_D = (int(x) for x in input().split(","))
     
 
     print(matrix1_D,"mart")
