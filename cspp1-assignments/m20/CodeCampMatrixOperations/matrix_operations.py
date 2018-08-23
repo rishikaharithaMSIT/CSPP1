ebafcd4d0ea661423ef3e3da15524cd2e1a6b8c7
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-23 10:36:31 +0530

    " -> 0 of 8 passed. pylint: 3.33/10 "

--- cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
+++ cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
@@ -17,7 +17,7 @@ def mult_matrix(matrix1_D, matrix1, matrix2_D,matrix2):
             h = 0
             for k in range(matrix1_D[1]):
                 h += matrix1[i][j]*matrix2[j][i]
-                print(matrix1[i][j], " ",matrix2[j][i])
+                print(matrix1[k][j], " ",matrix2[j][k])
             print(h)
         
 def add_matrix(matrix1_D, matrix1, matrix2_D,matrix2):
