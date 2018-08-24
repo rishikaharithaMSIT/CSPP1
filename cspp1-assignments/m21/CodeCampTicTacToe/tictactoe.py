c2894da58fcfaf702c290cde659e400ac276a97b
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-24 11:07:22 +0530

    " -> 14 of 14 passed. pylint: 4.67/10 "

--- cspp1-assignments/m21/CodeCampTicTacToe/tictactoe.py
+++ cspp1-assignments/m21/CodeCampTicTacToe/tictactoe.py
@@ -1,20 +1,3 @@
-def displayboard(board):
-    for i in board:
-        for j in i:
-            print(j ," " ,end='')
-        print()
-# def insertX(player1,board):   
-#   for i in range(len(board)):
-#       for j in range(len(board)):
-#           if i == player1[0] and j == player1[1] and board[i][j] == '_':
-#               board[i][j] = 'X'
-#   return board
-# def insertO(player2,board):
-#   for i in range(len(board)):
-#       for j in range(len(board)):
-#           if i == player2[0] and j == player2[1] and board[i][j] == '_':
-#               board[i][j] = 'O'
-#   return board
 def checkGame(board, player):
     game = False
     def checkRows(board, player):
@@ -37,7 +20,6 @@ def checkGame(board, player):
             diags = True 
         return diags
 
-
     rows = checkRows(board, player)
     cols = checkCols(board, player)
     diag = checkdiags(board, player)
@@ -52,7 +34,6 @@ def checkGame(board, player):
     elif rows == True or cols == True or diag == True:
         game = True
     return game
- 
 
 def main():
     board = []
@@ -64,8 +45,6 @@ def main():
             if j != 'x' and j != 'o' and j != '.':
                 invalid_input = True
 
-
-    #displayboard(board)
     x_game = checkGame(board, 'x')
     o_game = checkGame(board, 'o')
     if invalid_input == True:
@@ -80,30 +59,6 @@ def main():
             print("o")
     elif x_game == False and o_game == False:
         print("draw")
-
-
-    # while True:
-    #   player1 = input("Player 1 turn: ")
-    #   player1 = player1.split()
-    #   player1 = list(map(int, player1))
-    #   board  = insertX(player1, board)
-    #   displayboard(board)     
-    #   play1 = checkGame(board, 'X')
-    #   if play1 == True:
-    #       print("player 1 wins")
-    #       break
-    #   player2 = input("Player 2 turn: ")
-    #   player2 = player2.split()
-    #   player2 = list(map(int, player2))
-    #   board  = insertO(player2, board)
-    #   displayboard(board)
-    #   play2 = checkGame(board, '0')
-    #   if play2 == True:
-    #       print("player 2 wins")
-    #       break
-        
-
-
 
 if __name__ == "__main__":
     main()
