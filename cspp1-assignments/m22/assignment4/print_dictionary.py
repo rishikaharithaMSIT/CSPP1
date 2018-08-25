bfef2bf155cf71b91a9f3aa4b7f26092bc5a122b
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-25 10:59:06 +0530

    " -> 0 of 3 passed. pylint: 10.00/10 "

--- cspp1-assignments/m22/assignment4/print_dictionary.py
+++ cspp1-assignments/m22/assignment4/print_dictionary.py
@@ -17,7 +17,7 @@ def print_dictionary(dictionary):
 
 def main():
     '''in main()'''
-    dictionary = eval(input())
+    dictionary = input()
     print_dictionary(dictionary)
 
 if __name__ == '__main__':
