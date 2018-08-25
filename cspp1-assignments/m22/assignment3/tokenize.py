ef1ad9ca53a85a3053ab3191087c795c0367ca15
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-25 10:45:02 +0530

    " -> 3 of 4 passed. pylint: 7.89/10 "

--- cspp1-assignments/m22/assignment3/tokenize.py
+++ cspp1-assignments/m22/assignment3/tokenize.py
@@ -4,23 +4,23 @@ each word
 '''
 
 def tokenize(string):
-	token_dict = {}
+    token_dict = {}
     for each_line in string:
-    	line_words = each_line.split()
-    	for each_word in line_words:
-    		each_word = ''.join(e for e in each_word if e.isalpha())
-    		if each_word in token_dict:
-    			token_dict[each_word] += 1
-    		else:
-    			token_dict[each_word] = 1
+        line_words = each_line.split()
+        for each_word in line_words:
+            each_word = ''.join(e for e in each_word if e.isalpha())
+            if each_word in token_dict:
+                token_dict[each_word] += 1
+            else:
+                token_dict[each_word] = 1
     return token_dict
             
 def main():
     no_lines = int(input())
     in_str = []
     for each_line in range(no_lines):
-    	in_line = input()
-    	in_str.append(in_line)
+        in_line = input()
+        in_str.append(in_line)
     print(tokenize(in_str))
 
 if __name__ == '__main__':
