b00e9ba33ce9832604cb98eb9119df759952812c
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-25 10:40:38 +0530

    " -> 0 of 4 passed. pylint: 3.64/10 "

---
+++ cspp1-assignments/m22/assignment3/tokenize.py
@@ -0,0 +1,18 @@
+'''
+Write a function to tokenize a given string and return a dictionary with the frequency of
+each word
+'''
+
+def tokenize(string):
+    pass
+            
+def main():
+    no_lines = int(input())
+    in_str = []
+    for each_line in range(no_lines):
+    	in_line = input()
+    	in_str.append(in_line)
+    print(in_str)
+
+if __name__ == '__main__':
+    main()
