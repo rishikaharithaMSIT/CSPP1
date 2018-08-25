78ee6bec18e0e8b3ccc577129a306eeaf1ccd60c
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-08-25 10:58:42 +0530

    " -> 3 of 3 passed. pylint: 9.17/10 "

--- cspp1-assignments/m22/assignment4/print_dictionary.py
+++ cspp1-assignments/m22/assignment4/print_dictionary.py
@@ -5,15 +5,18 @@ the key and frequency with a SPACE - SPACE.
 '''
 
 def print_dictionary(dictionary):
+    '''print dictionary'''
     li_keys = []
     for each_key in dictionary:
         li_keys.append(each_key)
+
     li_keys.sort()
 
     for each_key in li_keys:
         print(each_key, "-", dictionary[each_key])
 
 def main():
+    '''in main()'''
     dictionary = eval(input())
     print_dictionary(dictionary)
 
